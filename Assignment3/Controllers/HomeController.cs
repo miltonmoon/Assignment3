7a31badcff9c22a05ac15e0e3310f25031a429a1
Shane Mmoon <moon.shanemilton@gmail.com>
2021-02-02 21:10:41 -0700

    upload please

---
+++ Assignment3/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EnterMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovie(ApplicationResponse appResponse)
+        {
+            TempStorage.AdddMovie(appResponse);
+            return View("Confirmation", appResponse);
+        }
+
+        public IActionResult Collection()
+        {
+            return View(TempStorage.Movies.Where(x => x.Name != "Independence Day"));
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assignment3/Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public static class TempStorage
+    {
+        private static List<ApplicationResponse> movies = new List<ApplicationResponse>();
+
+        public static IEnumerable<ApplicationResponse> Movies => movies;
+
+        public static void AdddMovie(ApplicationResponse newMovie)
+        {
+            movies.Add(newMovie);
+        }
+    }
+}
